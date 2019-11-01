58d42c8e3fd10d674b476ded4808b7fb8470f739
bajins <admin@bajins.com>
2019-11-01 16:22:51 +0800

    add encrypt

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"crypto/hmac"
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 	"golang.org/x/crypto/scrypt"
 )
@@ -27,4 +31,27 @@ func NewPass(passwd, salt string) (string, error) {
 	}
 
 	return hex.EncodeToString(dk), nil
+}
+
+// 计算hash1
+func ComputeHash1(message string, secret string) string {
+	h := hmac.New(sha1.New, []byte(secret))
+	h.Write([]byte(message))
+	// 转成十六进制
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// 计算HmacSha256
+func ComputeHmacSha256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	// 转成十六进制
+	return hex.EncodeToString(h.Sum(nil))
+
+}
+
+// 编码Base64
+func EncodeBase64(str string) string {
+	return base64.StdEncoding.EncodeToString([]byte(str))
 }
