c5ca3d38b2e500ecc1967ca0b639e832553b6cfd
woytu <admin@woytu.com>
2019-11-13 10:28:39 +0800

    allow

--- static/js/utils/time.js
+++ static/js/utils/time.js
@@ -14,16 +14,30 @@
 /**
  * 时间转换工具
  *
- * @param date          时间
- * @param join          年月日之间连接的字符
+ * @param date 时间
+ * @param join 年月日之间连接的字符
  * @returns {string}
  */
 const formatTime = (date, join) => {
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
-    return [year, month, day].map(formatNumber).join(join) + ' ' + [hour, minute, second].map(formatNumber).join(':');
+  let d = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate()
+  ];
+  let t = [
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
+  ];
+  let dateString = d.map((item, i, arr) => {
+    // 格式化日期，如月、日、时、分、秒保证为2位数
+    item = item.toString();
+    return item[1] ? item : '0' + item;
+  }).join(join);
+  let timeString = t.map((item, i, arr) => {
+    // 格式化日期，如月、日、时、分、秒保证为2位数
+    return item < 10 ? '0' + item : item;
+  }).join(":");
+  return dateString + ' ' + timeString;
 }
+
