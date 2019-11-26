3e1105b32e41d6350646fa0e3e6442678b6c5e96
bajins <admin@bajins.com>
2019-11-26 09:26:37 +0800

    fixes

--- static/js/utils/color.js
+++ static/js/utils/color.js
@@ -90,10 +90,10 @@ const randomHSLColor = function () {
  * @returns {boolean}
  */
 const isColor = function (value) {
-    const colorReg = /^#([a-fA-F0-9]){3}(([a-fA-F0-9]){3})?$/;
-    const rgbaReg = /^[rR][gG][bB][aA]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){3}\s*(\.|\d+\.)?\d+\s*\)$/;
-    const rgbReg = /^[rR][gG][bB]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){2}(25[0-5]|2[0-4]\d|1?\d{1,2})\s*\)$/;
-    const hslReg = /^[hH][sS][lL]\(([0-9]|[1-9][0-9]|[1-3][0-5][0-9]|360)\,(100|[1-9]\d|\d)(.\d{1,2})?%\,(100|[1-9]\d|\d)(.\d{1,2})?%\)$/;
+    let colorReg = /^#([a-fA-F0-9]){3}(([a-fA-F0-9]){3})?$/;
+    let rgbaReg = /^[rR][gG][bB][aA]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){3}\s*(\.|\d+\.)?\d+\s*\)$/;
+    let rgbReg = /^[rR][gG][bB]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){2}(25[0-5]|2[0-4]\d|1?\d{1,2})\s*\)$/;
+    let hslReg = /^[hH][sS][lL]\(([0-9]|[1-9][0-9]|[1-3][0-5][0-9]|360)\,(100|[1-9]\d|\d)(.\d{1,2})?%\,(100|[1-9]\d|\d)(.\d{1,2})?%\)$/;
 
-    return colorReg.test(value) || rgbaReg.test(value) || rgbReg.test(value);
+    return colorReg.test(value) || rgbaReg.test(value) || rgbReg.test(value) || hslReg.test(value);
 };
